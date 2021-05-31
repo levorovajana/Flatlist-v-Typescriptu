df4eb8d98e60f25cc68d53e009aef5c6e7692a8a
levorovajana <82833352+levorovajana@users.noreply.github.com>
2021-05-31 21:46:34 +0200

    Update sortButton.tsx
    Zrusila jsem tu zbytecne slozitou radici funkci. Vyresila jsem to pomoci reverse.

--- src/Components/sortButton.tsx
+++ src/Components/sortButton.tsx
@@ -1,43 +1,23 @@
 import React, {FC} from 'react';
 import {View,Button} from 'react-native';
-
+ 
 interface Props{
-    data: any [];
-    order: boolean;
-    updateData: (arg: any[]) => void
-    updateOrder: (arg: boolean) => void
+    [data: string]: any;
+    setData: (arg: string[]) => void;
   }
 
-const SortButton: FC <Props>  = ({data,order,updateData,updateOrder}) => {
-      
-      const sortData = () => {
-        if (order){
-          updateData([...data].sort(
-            (a: { episode_number: number; },
-            b: { episode_number: number; }) =>
-            a.episode_number > b.episode_number ? -1 
-            : a.episode_number < b.episode_number ? 1 : 0))
-        }       
-        else {
-          updateData([...data].sort(
-            (a: { episode_number: number; },
-            b: { episode_number: number; }) =>
-            a.episode_number < b.episode_number ? -1 
-            : a.episode_number > b.episode_number ? 1 : 0))
-          }
-          updateOrder(!order)           
-        }
-               
-    return(
-      <View style={{ flex: 1, padding: 24 }}>         
-        <View>
-            <Button 
-            title="Sort Movies" 
-            onPress={()=>sortData()}> 
-            </Button>  
-        </View>
+  const SortButton: FC <Props>  = ({data,setData}) => {
+             
+  return(
+    <View style={{ flex: 1, padding: 24 }}>         
+      <View>
+          <Button 
+          title="Sort Movies" 
+          onPress={()=>setData([...data].reverse())}> 
+          </Button>  
       </View>
-    )
+    </View>
+  )
 }
 
 export default SortButton;
